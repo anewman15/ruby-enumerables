880ee82d9c819344c2bf6d7762d8d5b56ac67241
Abdullah Numan <anewman15@hotmail.com>
2020-06-02 15:14:11 +0600

    Create my_select and my_any methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -36,4 +36,42 @@ module Enumerable
     end
     array_copy
   end
+
+  def my_all?(array)
+    array_size = array.length
+
+    block_return = Hash.new
+    i = 0
+    while i < array_size do
+      yield_value = yield array[i]
+      if block_return[yield_value] 
+        block_return[yield_value] += 1
+      else
+        block_return[yield_value] = 1
+      end
+      i += 1
+    end
+
+    if block_return[true] == array_size
+      true
+    else
+      false
+    end
+  end
+
+  def my_any?(array)
+    array_size = array.length
+
+    i = 0
+    while i < array_size do
+      if yield array[i]
+        output = true
+        break
+      else
+        output = false
+      end
+      i += 1
+    end
+    output
+  end
 end
