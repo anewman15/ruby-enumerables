1c453a5a8b0dac6fc34241aa000a2fd870c49059
Abdullah Numan <anewman15@hotmail.com>
2020-06-04 17:03:46 +0600

    Create my_inject and my_map_proc methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -125,4 +125,39 @@ module Enumerable
     end
     modified_object
   end
+
+  def my_inject(initial = nil)
+    
+    if initial
+      memo = initial
+      modified_object = my_map { |element| memo = yield memo, element }
+      modified_object[-1]
+    else
+      memo = first
+      modified_object = to_a[1..-1].my_map { |element| memo = yield memo, element }
+      modified_object[-1]
+    end
+  end
+
+  def multiply_els(array)
+    array.my_inject { |memo, item| memo * item }
+  end
+
+  def my_map_proc(a_proc = nil)
+    modified_object = []
+
+    i = 0
+    if a_proc
+      while i < length
+        modified_object << a_proc.call(self[i])
+        i += 1
+      end
+    else
+      while i < length do
+        modified_object << (yield self[i])
+        i += 1
+      end
+    end
+    modified_object
+  end
 end
