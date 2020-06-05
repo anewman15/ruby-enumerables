f40607672e0db581ffc9e1148cca1f368ed3d988
Abdullah Numan <anewman15@hotmail.com>
2020-06-05 22:06:34 +0600

    Fix rubocop errors

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -4,7 +4,7 @@ module Enumerable
 
     i = 0
     if block_given?
-      while i < length do
+      while i < length
         modified_object << (yield self[i])
         i += 1
       end
@@ -19,7 +19,7 @@ module Enumerable
 
     i = 0
     if block_given?
-      while i < length do
+      while i < length
         modified_object << (yield self[i], i)
         i += 1
       end
@@ -34,10 +34,8 @@ module Enumerable
 
     i = 0
     if block_given?
-      while i < length do
-        if (yield self[i]) == true
-          modified_object << self[i]
-        end
+      while i < length
+        modified_object << self[i] if (yield self[i]) == true
         i += 1
       end
       modified_object
@@ -47,11 +45,11 @@ module Enumerable
   end
 
   def my_all?(pattern = nil)
-    decision = true if length == 0 
+    decision = true if length.zero?
 
-    i = 0 
+    i = 0
     if block_given?
-      while i < length do
+      while i < length
         unless yield self[i]
           decision = false
           break
@@ -61,7 +59,7 @@ module Enumerable
         end
       end
     elsif !block_given? && pattern
-      while i < length do
+      while i < length
         unless pattern === self[i]
           decision = false
           break
@@ -71,8 +69,8 @@ module Enumerable
         end
       end
     elsif !block_given? && !pattern
-      while i < length do
-        unless self[i] 
+      while i < length
+        unless self[i]
           decision = false
           break
         else
@@ -87,11 +85,11 @@ module Enumerable
   end
 
   def my_any?(pattern = nil)
-    decision = false if length == 0 
+    decision = false if length.zero?
 
-    i = 0 
+    i = 0
     if block_given?
-      while i < length do
+      while i < length
         if yield self[i]
           decision = true
           break
@@ -101,7 +99,7 @@ module Enumerable
         end
       end
     elsif !block_given? && pattern
-      while i < length do
+      while i < length
         if pattern === self[i]
           decision = true
           break
@@ -111,8 +109,8 @@ module Enumerable
         end
       end
     elsif !block_given? && !pattern
-      while i < length do
-        if self[i] 
+      while i < length
+        if self[i]
           decision = true
           break
         else
@@ -127,11 +125,11 @@ module Enumerable
   end
 
   def my_none?(pattern = nil)
-    decision = true if length == 0 
+    decision = true if length.zero?
 
-    i = 0 
+    i = 0
     if block_given?
-      while i < length do
+      while i < length
         if yield self[i]
           decision = false
           break
@@ -141,7 +139,7 @@ module Enumerable
         end
       end
     elsif !block_given? && pattern
-      while i < length do
+      while i < length
         if pattern === self[i]
           decision = false
           break
@@ -151,8 +149,8 @@ module Enumerable
         end
       end
     elsif !block_given? && !pattern
-      while i < length do
-        if self[i] 
+      while i < length
+        if self[i]
           decision = false
           break
         else
@@ -171,7 +169,7 @@ module Enumerable
     i = 0
     counter = 0
     if block_given?
-      while i < length do
+      while i < length
         if yield self[i]
           counter += 1
         else
@@ -181,7 +179,7 @@ module Enumerable
       end
       counter
     elsif !block_given? && value
-      while i < length do
+      while i < length
         if self[i] == value
           counter += 1
         else
@@ -197,27 +195,56 @@ module Enumerable
 
   def my_map
     modified_object = []
-    object_size = self.length
 
     i = 0
-    while i < object_size do
-      modified_object << (yield self[i])
-      i += 1
+    if block_given?
+      while i < length
+        modified_object << (yield self[i])
+        i += 1
+      end
+      modified_object
+    else
+      to_enum
     end
-    modified_object
   end
 
-  def my_inject(initial = nil)
-    
-    if initial
-      memo = initial
-      modified_object = my_map { |element| memo = yield memo, element }
-      modified_object[-1]
-    else
-      memo = first
-      modified_object = to_a[1..-1].my_map { |element| memo = yield memo, element }
-      modified_object[-1]
+  # def my_inject(initial = nil, sym = nil)
+  #   modified_object = []
+
+  #   if block_given?
+  #     if initial
+  #       memo = initial
+  #       modified_object = my_map { |element| memo = yield memo, element }
+  #     else
+  #       memo = first
+  #       modified_object = to_a[1..-1].my_map { |element| memo = yield memo, element }
+  #     end
+  #   elsif !block_given? && sym
+  #     if initial
+  #       memo = initial
+  #       modified_object = memo
+  #     end
+  #   end
+  #   modified_object[-1]
+  # end
+
+  def my_injectt(init = nil, sym = nil)
+    if block_given?
+      if !init.nil?
+        num = init
+        my_each { |elem| num = yield(num, elem) }
+      else
+        num = first
+        to_a[1..-1].my_each { |elem| num = yield(num, elem) }
+      end
+    elsif !sym.nil?
+      num = init
+      my_each { |elem| num = num.send(sym, elem) }
+    elsif !init.nil?
+      num = first
+      to_a[1..-1].my_each { |elem| num = num.send(init, elem) }
     end
+    num
   end
 
   def multiply_els(array)
